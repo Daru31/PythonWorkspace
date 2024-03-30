9806cdbbb1daf0954ce9449bdb9f0acc2ae0f798
Daru31 <dalbit3189@naver.com>
2024-03-30 21:39:49 +0900

    ignore file & folder config

--- practice.py
+++ practice.py
@@ -1,3 +1,8 @@
-starbucks = ["A", "B", "C"]
-for customer in starbucks:
-    print("{0}, Hello.".format(customer))
+# while 
+customer = "토르"
+index = 5
+while index >= 1:
+    print("{0}님, 커피가 준비되었습니다. {1}번 남았어요.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기처분되었습니다.")
