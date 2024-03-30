012c971d4624403b3dbadf93de213c738693631d
Daru31 <dalbit3189@naver.com>
2024-03-30 21:44:18 +0900

    ignore Practice.py file

--- practice.py
+++ practice.py
@@ -5,4 +5,4 @@ while index >= 1:
     print("{0}님, 커피가 준비되었습니다. {1}번 남았어요.".format(customer, index))
     index -= 1
     if index == 0:
-        print("커피는 폐기처분되었습니다.")
+        print("커피는 폐기처분되었습니다.") 
