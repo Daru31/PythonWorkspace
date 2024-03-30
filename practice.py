32529779e7e1fc1b2ab0fb5733631da1ff53032e
Daru31 <dalbit3189@naver.com>
2024-03-30 21:51:35 +0900

    deleted practice.py file

--- practice.py
+++
@@ -1,8 +0,0 @@
-# while 
-customer = "토르"
-index = 5
-while index >= 1:
-    print("{0}님, 커피가 준비되었습니다. {1}번 남았어요.".format(customer, index))
-    index -= 1
-    if index == 0:
-        print("커피는 폐기처분되었습니다.") 
