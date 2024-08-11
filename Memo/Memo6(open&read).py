57481c58977efc510f525154b75748f08ec92d4d
Daru <dalbit3189@naver.com>
2024-08-11 19:02:10 +0900

    Modified Memo6(open&read).py

--- Memo/Memo6(open&read).py
+++ Memo/Memo6(open&read).py
@@ -12,6 +12,13 @@ py = open("helloworld.py", "r", encoding="utf8") # 3rd
 while True: # 파일의 길이를 모를 때  
     line = py.readline() # 한줄씩 읽다가 
     if not line: 
+        print("")
         break # 줄에 아무 내용이 없으면 종료 
     print(line, end="") 
-py.close()
+py.close() 
+
+py = open("helloworld.py", "r", encoding="utf8") # 4th 
+lines = py.readlines() # 각 줄을 인자로 해서 리스트에 담아둠 
+for line in lines: 
+    print(line, end="") # 하나씩 빼오기 
+py.close() 
